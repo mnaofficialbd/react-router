84d9fd06cdf657f954633fa8f7cf317bce19f7b1
mnaofficial <mna.official92@gmail.com>
2022-04-01 00:35:50 +0600

    friends component

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([])
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.json())
+            .then(data => setFriends(data))
+    }, [])
     return (
         <div>
-            <h3>Friends component</h3>
+            <h3>Hello Friends. How are you.</h3>
+            <p>Give me some money...: {friends.length}</p>
+            {
+                friends.map(friend => 
+                <Friend
+                    key={friend.id}
+                    friend={friend}
+                ></Friend>)
+            }
         </div>
     );
 };
