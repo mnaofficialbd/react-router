a21197abe3cd2c0fa1a89b24f4593f52e5404b99
mnaofficial <mna.official92@gmail.com>
2022-04-01 00:14:41 +0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Friends from './components/Friends/Friends';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h2>React Router</h2>
+      <h2>Welcome to my fancy Routing website!!</h2>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/friends' element={<Friends/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3>Not found your page</h3>
+            <h2>404</h2>
+        </div>
+    );
+};
+
+export default NotFound;
