8d478174724395d165d7b31be71c6438347adf08
mnaofficial <mna.official92@gmail.com>
2022-04-01 09:51:47 +0600

    header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Friends from './components/Friends/Friends';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <img src="https://mnaofficialbd.files.wordpress.com/2020/03/cropped-mna.png?w=1024&h=182" alt="" />
       
       <Routes>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to Routing website</h2>
+            <nav>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/friends">Friends</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
