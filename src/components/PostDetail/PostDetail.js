0d796d2978456db9318969a5462039ab67e9a5e2
mnaofficial <mna.official92@gmail.com>
2022-04-01 14:09:06 +0600

    complete

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const PostDetail = () => {
-    const {postId}=useParams();
-    // console.log(postId);
+    const { postId } = useParams();
+    const [post, setPost] = useState({})
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/posts/${postId}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [postId])
+
     return (
         <div>
             <h4>Post Detail: {postId}</h4>
+            <h5>Title: {post.title}</h5>
+            <p>{post.body}</p>
         </div>
     );
 };
